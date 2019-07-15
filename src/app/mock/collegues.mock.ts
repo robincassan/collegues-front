55353be45b1162cb2014c74787c3565646312d56
Robin CASSAN <robin.cassan@netc.eu>
2019-07-15 17:47:45 +0200

    realisation interface html en cours

---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,3 @@
+import { Collegue } from '../models/Collegue';
+
+export const col = new Collegue('4523hh', 'Broquerie', 'Jean','Jeanbroq@gmail.fr', '05/05/2005', 'https://upload.wikimedia.org/wikipedia/en/1/16/Crash_Bandicoot.png' );
---
+++ src/app/models/Collegue.ts
@@ -0,0 +1,9 @@
+export class Collegue {
+
+   
+
+    constructor(public matricule:string, public nom: string, public prenom: string, public email: string, public dateDeNaissance: Date,
+         public photoUrl: string ){             
+    }
+  
+}
